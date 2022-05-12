820adef1801b42118b48a998104feb35b54dafba
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-05-11 21:35:13 -0700

    Removed dbl quotes in js

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # LAB 0-1
+
 ## Project Name: About Me
+
 ## Author Name: John Rayome
 
-
 I built this site to ask questions to the user before they gain access to the information. This was build with HTML, CSS, and JavaScript.
-
 
 'For loop' assistance by TA Justin Hamerly with Dom G @ Table 2
--- js/app.js
+++ js/app.js
@@ -9,10 +9,12 @@ if (answerOne === 'yes' || answerOne === 'y') {
   // console.log(userName + ', you are correct!');
   alert(userName + ', you are correct!');
   score++;
+  console.log(answerOne + ' - ' + score + '\'s questions right');
 } else if (answerOne === 'no' || answerOne === 'n') {
   // console.log('That\'s not correct ' + userName);
   alert('That\'s not correct ' + userName);
 }
+
 
 let answerTwo = prompt('Was I born in in New York?').toLowerCase();
 if (answerTwo === 'yes' || answerTwo === 'y') {
@@ -22,13 +24,16 @@ if (answerTwo === 'yes' || answerTwo === 'y') {
   // console.log(userName + ', you are correct!');
   alert(userName + ', you are correct!');
   score++;
+  console.log(answerTwo + ' - ' + score +' questions right');
 }
+
 
 let answerThree = prompt('Did I serve in the Air Force?').toLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
   // console.log(userName + ', you are correct!')
   alert(userName + ', you are correct!');
   score++;
+  console.log(answerThree + ' - ' + score +'\'s questions right');
 } else if (answerThree === 'no' || answerThree === 'n') {
   // console.log('That\'s not correct ' + userName);
   alert('That\'s not correct');
@@ -42,12 +47,14 @@ if (answerFour === 'yes' || answerFour === 'y') {
   // console.log(userName + ', you are correct!')
   alert(userName + ', you are correct!');
   score++;
+  console.log(answerFour + ' - ' + score +' questions right');
 }
 
 let answerFive = prompt('Are the Mets my favorite team in the MLB?').toLowerCase();
 if (answerFive === 'yes' || answerFive === 'y') {
   console.log(userName + ', you are correct!');
   score++;
+  console.log(answerFive + ' - ' + score +' questions right');
   alert(userName + ', you are correct!');
 } else if (answerFive === 'no' || answerFive === 'n') {
   // console.log('That\'s not correct ' + userName);
@@ -73,6 +80,7 @@ while (userAnswer !== correctAnswer) {
   } else if (userAnswer === correctAnswer) {
     alert('You guessed right, ' + userName + '!');
     score++;
+    console.log(userAnswer + ' - ' + score +' questions right');
     break;
   }
   attempts = (attempts - 1);
@@ -91,18 +99,22 @@ while (userCountry !== countries[0] || userCountry !== countries[1] || userCount
   if (userCountry === countries[0]) {
     alert('That\'s right. I have been to ' + countries[0]);
     score++;
+    console.log(userCountry + ' - ' + score +' questions right');
     break;
   } else if (userCountry === countries[1]) {
     alert('That\'s right. I have been to ' + countries[1]);
     score++;
+    console.log(userCountry + ' - ' + score +' questions right');
     break;
   } else if (userCountry === countries[2]) {
     alert('That\'s right. I have been to ' + countries[2]);
     score++;
+    console.log(userCountry + ' - ' + score +' questions right');
     break;
   } else if (userCountry === countries[3]) {
     alert('That\'s right. I have been to ' + countries[3]);
     score++;
+    console.log(userCountry + ' - ' + score +' questions right');
     break;
   } else {
     alert('I\'ve never been to ' + userCountry + '.');
